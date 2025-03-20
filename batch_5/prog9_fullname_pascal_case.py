1851a8a76796ae706f6c90d72ab13f2df8cdf019
kok0krunch <amaliakadoi@gmail.com>
2025-03-20 19:09:15 +0800

    Format into a proper case and split into words

--- batch_5/prog9_fullname_pascal_case.py
+++ batch_5/prog9_fullname_pascal_case.py
@@ -2,3 +2,6 @@
 
 # Ask user to input their fullname
 fullname = input("Enter your fullname: ")
+
+# Format fullname in a proper case and split it by words.
+proper_fullname = fullname.title().split()
