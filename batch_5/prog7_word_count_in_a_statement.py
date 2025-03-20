54134ab9f03dbd4743eb3eef9d401d8eafb994c5
kok0krunch <amaliakadoi@gmail.com>
2025-03-20 18:51:51 +0800

    Ask user to input statement

---
+++ batch_5/prog7_word_count_in_a_statement.py
@@ -0,0 +1,4 @@
+# Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+# Ask the user to input a complete statement
+statement = input("Enter a statement: ")
