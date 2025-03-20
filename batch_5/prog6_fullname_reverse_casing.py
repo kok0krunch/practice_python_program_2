0717d74525e8c56615c1d4514c81ccba39a16369
kok0krunch <amaliakadoi@gmail.com>
2025-03-20 18:48:08 +0800

    Ask user to input fullname

---
+++ batch_5/prog6_fullname_reverse_casing.py
@@ -0,0 +1,5 @@
+# Create a program that ask the user to input their fullname in incorrect casing. 
+# Print each character of the input in reverse casing.
+
+# Ask user to input their fullname
+fullname = input("Enter your fullname: ")
