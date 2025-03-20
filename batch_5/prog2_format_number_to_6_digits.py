b4627d03e1829b214fa9389d3bca1da3981605bb
kok0krunch <amaliakadoi@gmail.com>
2025-03-20 18:23:52 +0800

    removed int and used zfill() to format number to 6 digits

--- batch_5/prog2_format_number_to_6_digits.py
+++ batch_5/prog2_format_number_to_6_digits.py
@@ -2,5 +2,7 @@
 # Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
 # Ask user to input a number
-number = int(input("Enter a number (0-1000): "))
+number = input("Enter a number (0-1000): ")
 
+# Format the nuber to 6 digit with leading zeros
+formatted_number = number.zfill(6)
