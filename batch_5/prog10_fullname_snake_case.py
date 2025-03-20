5b84025130e08c5b7d355e0f3e1a24313ba12420
kok0krunch <amaliakadoi@gmail.com>
2025-03-20 19:15:29 +0800

    Format to lower case and split by words

--- batch_5/prog10_fullname_snake_case.py
+++ batch_5/prog10_fullname_snake_case.py
@@ -2,3 +2,6 @@
 
 # Ask user to input their fullname
 fullname = input("Enter your fullname: ")
+
+# Format fullname in a lower case and split it by words.
+lower_fullname = fullname.lower().split()
