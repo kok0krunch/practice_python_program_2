ceca10b6fe52852797abc30e6dc1e5ba722fe57c
kok0krunch <amaliakadoi@gmail.com>
2025-03-20 18:07:08 +0800

    Ask user to input full name

---
+++ batch_5/prog1_fullname_remove_leading_spaces.py
@@ -0,0 +1,4 @@
+# Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+# Ask user to input their fullname with leading spaces
+fullname = input("Enter your full name with leading spaces: ")
