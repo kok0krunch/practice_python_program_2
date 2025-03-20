1035a083c334756a66c6d591e5b451e1fbe7caf3
kok0krunch <amaliakadoi@gmail.com>
2025-03-20 18:18:46 +0800

    Ask user to input number

---
+++ batch_5/prog2_format_number_to_6_digits.py
@@ -0,0 +1,6 @@
+# Create a program that ask the user to input a number (0-1000). 
+# Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+# Ask user to input a number
+number = int(input("Enter a number (0-1000): "))
+
