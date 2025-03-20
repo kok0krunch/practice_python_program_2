b0280177b3296dd4211a80fea05122bd9395e297
kok0krunch <amaliakadoi@gmail.com>
2025-03-20 18:10:12 +0800

    Change 1st variable and add used string method to remove leading spaces

--- batch_5/prog1_fullname_remove_leading_spaces.py
+++ batch_5/prog1_fullname_remove_leading_spaces.py
@@ -1,4 +1,7 @@
 # Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 # Ask user to input their fullname with leading spaces
-fullname = input("Enter your full name with leading spaces: ")
+name = input("Enter your full name with leading spaces: ")
+
+# Remove leading spaces using lstrip() method
+fullname = name.lstrip()
