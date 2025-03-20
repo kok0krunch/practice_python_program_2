244534d01724e94cd77b11bd16edf161780567fa
kok0krunch <amaliakadoi@gmail.com>
2025-03-20 18:55:59 +0800

    Ask user to input fullname

---
+++ batch_5/prog8_count_characters_fullname.py
@@ -0,0 +1,4 @@
+# Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+# Ask user to input their fullname
+fullname = input("Enter your fullname: ")
