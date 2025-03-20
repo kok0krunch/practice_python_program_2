6c614ea944061e88623252a22ca1e52c5a1ed3ee
kok0krunch <amaliakadoi@gmail.com>
2025-03-20 19:14:24 +0800

    Ask user to input fullname

---
+++ batch_5/prog10_fullname_snake_case.py
@@ -0,0 +1,4 @@
+# Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
+# Ask user to input their fullname
+fullname = input("Enter your fullname: ")
